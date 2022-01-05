bff8097429c37160db81e443162373aadc440006
Andres Giraldo <andresganc@gmail.com>
2022-01-05 10:38:32 -0500

    New: Jest

---
+++ course-nc/jest/test/index.test.js
@@ -0,0 +1,25 @@
+
+const sum = require('../src/index');
+//import sum from '../src/index.js'
+
+describe('Test for sum', () => {
+
+    test('1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+    it('Should return 3 with 1 + 2', () => {
+        expect(sum(1, 2)).toBe(3)
+    })
+
+    test('1 + (-2) to equal -1', () => {
+        expect(sum(1, -2)).toBe(-1)
+    })
+
+    test('-1 + (-2) to equal -3', () => {
+        expect(sum(-1, -2)).toBe(-3)
+    })
+
+})
+
+
---
+++ documentation/doc/jest/install.md
@@ -0,0 +1,6 @@
+
+## INSTALL JEST 
+
+npm install --save-dev jest
+
+yarn add --dev jest
